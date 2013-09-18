4731b04585fb713be0749ea3d749b1e278c3e77b
FilmCoder <dominic.napoleon@gmail.com>
2013-09-18 17:36:55 -0400

    yo yo i just commited what a terrible useless comment

---
+++ EarthquakeHeapNode.java
@@ -0,0 +1,17 @@
+
+public class EarthquakeHeapNode
+    implements HeapAwareNode<T>
+{
+
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
